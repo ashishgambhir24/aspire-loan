f1583c498c00922bdb4bbea9c5a9752410cc5cd1
Ashish Gambhir <ashishgambhir@Ashishs-MacBook-Pro.local>
2023-05-28 16:03:06 +0530

    loan and installments models added with create loan functionality

---
+++ loan_backend/lib/validators.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import ValidationError
+
+def validate_nonzero(value):
+    if value == 0:
+        raise ValidationError(
+            _('Quantity %(value)s is not allowed'),
+            params={'value': value},
+        )
